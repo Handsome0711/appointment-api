018c54d66515f42f28b5fd660b67030c7f95c707
vladyslav <vlad.bes0711@gmail.com>
2020-07-01 14:35:46 +0300

    added security configs, authorization and authentication with jwt token

---
+++ src/main/java/com/example/appointmentapi/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,60 @@
+package com.example.appointmentapi.filter;
+
+import com.example.appointmentapi.security.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        if (authentication == null) {
+            chain.doFilter(request, response);
+            return;
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(JwtProperties.TOKEN_HEADER);
+        if (!StringUtils.isEmpty(token) && token.startsWith(JwtProperties.TOKEN_PREFIX)) {
+            byte[] signingKey = JwtProperties.JWT_SECRET.getBytes();
+
+            Jws<Claims> parsedToken = Jwts.parser()
+                    .setSigningKey(signingKey)
+                    .parseClaimsJws(token.replace("Bearer ", ""));
+            String username = parsedToken
+                    .getBody()
+                    .getSubject();
+            Set<SimpleGrantedAuthority> roles = ((List<?>) parsedToken.getBody()
+                    .get("role")).stream()
+                    .map(authority -> new SimpleGrantedAuthority((String) authority))
+                    .collect(Collectors.toSet());
+            if (!StringUtils.isEmpty(username)) {
+                return new UsernamePasswordAuthenticationToken(username, null, roles);
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/example/appointmentapi/model/Role.java
@@ -0,0 +1,45 @@
+package com.example.appointmentapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+    public Role() {
+
+    }
+
+    private Role(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public static Role of(String roleName) {
+        return new Role(RoleName.valueOf(roleName));
+    }
+
+    public enum RoleName {
+        TEACHER, STUDENT
+    }
+}
